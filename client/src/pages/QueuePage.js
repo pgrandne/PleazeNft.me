b8e90472703ede077650e0f1e60dc39346822cea
Perrin <perrin.grandne@protonmail.com>
2022-08-19 16:39:26 +0200

    Adding link to polygon scan

--- client/src/pages/QueuePage.js
+++ client/src/pages/QueuePage.js
@@ -43,7 +43,10 @@ const QueuePage = () => {
                     ))}
                 </tbody>
             </Table>
-            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+            <br /><br />
+            <p>The address of the smart contract is : 0x1aD037Edc7758114FC80753DA2DBF246e14AdaED</p>
+            <p>You can check all information by clicking on this <a href="https://mumbai.polygonscan.com/address/0x1aD037Edc7758114FC80753DA2DBF246e14AdaED">link (polygon scan)</a></p>
+            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
         </Container>
     )
 }
